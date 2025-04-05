5151f6eeaa9cb281ee54a2e83d3bff53eae27b90
Edwin Juarez <edwinjuarez24x@gmail.com>
2025-04-04 23:04:15 -0500

    Interactuando con la base de datos con el Query Builder de Laravel

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -3,11 +3,20 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class ProductController extends Controller
 {
     public function index()
     {
+        // Query Builder
+        // $products = DB::table('products')->get();
+        // dd($products);
+
+        // Raw SQL Queries (Consultas SQL crudas)
+        $products = DB::select('select * from products');
+        dd($products);
+
         // return 'This is the list of products from CONTROLLER';
         return view('products.index');
     }
@@ -24,6 +33,16 @@ class ProductController extends Controller
 
     public function show($product)
     {
+        // Query Builder
+        // $product = DB::table('products')->where('id', $product)->get();
+        // $product = DB::table('products')->where('id', $product)->first();
+        // $product = DB::table('products')->find($product);
+        // dd($product);
+
+        // Raw SQL Queries (Consultas SQL crudas)
+        $product = DB::select('select * from products where id = ?', [$product]);
+        dd($product);
+
         // return "Showing product with id: {$product} from CONTROLLER";
         return view('products.show');
     }
